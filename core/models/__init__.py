4828e24ac6aed617cc01a3b224e25e32aad72409
Yaroslav <yarikbritov@yandex.ru>
2023-11-19 11:54:57 +0300

    create user model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "Product",
     "DatabaseHelper",
     "db_helper",
+    "User",
 )
 
 
 from .base import Base
 from .product import Product
 from .db_helper import db_helper, DatabaseHelper
+from .user import User
--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import uvicorn
 from fastapi import FastAPI
 
 from core.config import settings
-from core.models import Base, db_helper
 from api_v1 import router as router_v1
 from users.views import router as users_router
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan():
     yield
 
 
